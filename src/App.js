86d1ab91207276a69b3f0e04d74699fce2d8fa65
Ahmad Izzudin Alifyandra <alifyandra@yahoo.co.uk>
2021-01-20 19:57:10 +0700

    Added states

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import StreamNavbar from "./components/Navbar";
 import Header from "./components/Header";
-import axion from "axion";
+import axios from "axios";
 const API_URL = "https://api.themoviedb.org/3";
 
+const fetchMovies = (url) => {};
+
 function App() {
+  const [ownedMovies, setOwnedMovies] = useState([]);
+  const [balance, setBalance] = useState(100000);
+  const [movieList, setMovieList] = useState([]);
+
   function getPageQuery() {
     let search = window.location.search;
     let params = new URLSearchParams(search);
-    return params.get("query");
+    return params.get("page") ? params.get("page") : 1;
   }
-
-  console.log(getPageQuery());
 
   return (
     <Router>
@@ -20,6 +24,7 @@ function App() {
         <Switch>
           <Route path="/">
             <Header />
+            <MovieList ownedMovies={ownedMovies} />
           </Route>
         </Switch>
       </div>
