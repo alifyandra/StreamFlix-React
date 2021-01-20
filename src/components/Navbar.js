c6fadb0ace2d8d77faa1b289d9d3b79a85d25f08
Ahmad Izzudin Alifyandra <alifyandra@yahoo.co.uk>
2021-01-20 22:13:29 +0700

    Added balance to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
-import { Navbar } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const StreamNavbar = () => {
+const StreamNavbar = ({ balance }) => {
   return (
     <>
       <Navbar bg="dark" variant="dark" expand="sm">
         <Link to="/">
           <Navbar.Brand>StreamFlix</Navbar.Brand>
         </Link>
+        <Nav className="mr-auto"></Nav>
+        <Nav>
+          <Nav.Item style={{ color: "white", fontWeight: "400" }}>
+            Balance: Rp. {balance}
+          </Nav.Item>
+        </Nav>
       </Navbar>
     </>
   );
