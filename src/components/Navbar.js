6f532599bc8b68aa9e0a1d480ef69ad9547c2118
Ahmad Izzudin Alifyandra <alifyandra@yahoo.co.uk>
2021-01-20 15:53:55 +0700

    Added react-bootstrap Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const StreamNavbar = () => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" expand="sm">
+        <Link to="/">
+          <Navbar.Brand>StreamFlix</Navbar.Brand>
+        </Link>
+      </Navbar>
+    </>
+  );
+};
+
+export default StreamNavbar;
