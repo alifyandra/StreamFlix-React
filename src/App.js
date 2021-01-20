a46622c8b35c92f1f860931cfc971564b3a1ea0e
Ahmad Izzudin Alifyandra <alifyandra@yahoo.co.uk>
2021-01-20 17:53:30 +0700

    Added Header for main page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import StreamNavbar from "./components/Navbar";
-import "./App.css";
+import Header from "./components/Header";
+import axion from "axion";
+const API_URL = "https://api.themoviedb.org/3";
 
 function App() {
+  function getPageQuery() {
+    let search = window.location.search;
+    let params = new URLSearchParams(search);
+    return params.get("query");
+  }
+
+  console.log(getPageQuery());
+
   return (
     <Router>
       <StreamNavbar />
-      <div className="container"></div>
+      <div className="container">
+        <Switch>
+          <Route path="/">
+            <Header />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="container" style={{ padding: "1em" }}>
+      <h3 style={{ textAlign: "center" }}>Movies Now Playing in Indonesia</h3>
+    </div>
+  );
+};
+
+export default Header;
